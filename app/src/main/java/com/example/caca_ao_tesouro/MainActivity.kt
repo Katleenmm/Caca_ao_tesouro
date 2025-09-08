29b3bfb9b9c4b7d8846790a73c5fb806ab820d7e
“Katleenmm” <katleen.mendes@outlook.com>
2025-09-08 01:03:46 -0300

    Adicionei botões e perguntas

--- app/src/main/java/com/example/caca_ao_tesouro/MainActivity.kt
+++ app/src/main/java/com/example/caca_ao_tesouro/MainActivity.kt
@@ -3,24 +3,25 @@ package com.example.caca_ao_tesouro
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.caca_ao_tesouro.ui.theme.Caca_ao_tesouroTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,21 +29,42 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             val meuNavController = rememberNavController()
-            NavHost(navController = meuNavController, startDestination = "Tela01"){
+
+            NavHost(navController = meuNavController, startDestination = "TelaInicial") {
                 composable("TelaInicial") {
-                    TelaInicial(onNavEnigma01 ={meuNavController.navigate("Enigma01")})
+                    TelaInicial(onNavEnigma01 = {
+                        meuNavController.navigate("Enigma01")
+                    })
                 }
                 composable("Enigma01") {
-                    Enigma01()
+                    Enigma01(onNavEnigma02 = {
+                        meuNavController.navigate("Enigma02")
+                    },onVoltar = {
+                        meuNavController.popBackStack()
+                    })
                 }
                 composable("Enigma02") {
-                    Enigma02()
+                    Enigma02(onNavEnigma03 = {
+                        meuNavController.navigate("Enigma03")
+                    },onVoltar = {
+                        meuNavController.popBackStack()
+                    }
+                    )
                 }
                 composable("Enigma03") {
-                    Enigma03()
+                    Enigma03(onNavTesouro = {
+                        meuNavController.navigate("Tesouro")
+                    },onVoltar = {
+                        meuNavController.popBackStack()
+                    })
                 }
                 composable("Tesouro") {
-                    Tesouro()
+                    Tesouro(onVoltarTelaInicial = {
+                        meuNavController.navigate("TelaInicial") {
+                            // Remove o backstack para evitar que o usuário volte com o botão "voltar"
+                            popUpTo("TelaInicial") { inclusive = true }
+                        }
+                    })
                 }
             }
         }
@@ -50,55 +72,172 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun TelaInicial(onNavEnigma01: () -> Unit){
+fun TelaInicial(onNavEnigma01: () -> Unit) {
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
-    ){
+    ) {
         Text(
-            text = "Caça ao Tesouro",
-            fontSize = 24.sp
+            text = "Caça ao Tesouro \n \n - KIMETSU NO YAIBA -",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .background(Color.LightGray)
+                .padding(24.dp)
+                .fillMaxWidth()
         )
-        Button(onClick = {onNavEnigma01}) {
-            Text("Começar!")
+
+        Spacer(modifier = Modifier.padding(16.dp))
+
+        Button(onClick = { onNavEnigma01() }) {
+            Text("Começar!", fontSize = 18.sp)
         }
     }
 }
 
-fun Enigma01(){
+@Composable
+fun Enigma01(onNavEnigma02: () -> Unit, onVoltar: () -> Unit) {
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
-    ){
+    ) {
         Text(
-            text = "Enigma01",
+            text = "Enigma 01",
             fontSize = 24.sp
         )
-        Button(onClick = {onNavEnigma02}) {
-            Text("!")
-        }
-        Button(onClick = {onNavEnigma02}) {
-            Text("!")
+        Spacer(modifier = Modifier.padding(24.dp))
+
+        Text(
+            text = "Qual o nome da técnica secreta da Respiração do Sol, passada na família Kamado como uma dança tradicional?",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .background(Color.LightGray)
+                .padding(8.dp)
+        )
+
+        Spacer(modifier = Modifier.padding(24.dp))
+
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(onClick = { onVoltar() }) {
+                Text("Voltar", fontSize = 18.sp)
+            }
+
+            Button(onClick = { onNavEnigma02() }) {
+                Text("Próximo Enigma", fontSize = 18.sp)
+            }
         }
     }
 }
 
-fun Enigma02(){
+@Composable
+fun Enigma02(onNavEnigma03: () -> Unit,  onVoltar: () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Enigma 02",
+            fontSize = 24.sp
+        )
 
+        Spacer(modifier = Modifier.padding(24.dp))
+
+        Text(
+            text = "Qual é o verdadeiro nome do Hashira do Som?",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .background(Color.LightGray)
+                .padding(8.dp)
+        )
+
+        Spacer(modifier = Modifier.padding(24.dp))
+
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(onClick = { onVoltar() }) {
+                Text("Voltar", fontSize = 18.sp)
+            }
+
+            Button(onClick = { onNavEnigma03() }) {
+                Text("Próximo Enigma", fontSize = 18.sp)
+            }
+        }
+    }
 }
 
-fun Enigma03(){
+@Composable
+fun Enigma03(onNavTesouro: () -> Unit, onVoltar: () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Enigma 03",
+            fontSize = 24.sp
+        )
+        Spacer(modifier = Modifier.padding(24.dp))
 
+        Text(
+            text = "Por que as espadas dos caçadores de onis são chamadas de Nichirin?",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .background(Color.LightGray)
+                .padding(8.dp)
+        )
+
+        Spacer(modifier = Modifier.padding(24.dp))
+
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(onClick = { onVoltar() }) {
+                Text("Voltar",fontSize = 18.sp)
+            }
+
+            Button(onClick = { onNavTesouro() }) {
+                Text("Finalizar!",fontSize = 18.sp)
+            }
+        }
+    }
 }
 
-fun Tesouro(){
-
+@Composable
+fun Tesouro(onVoltarTelaInicial: () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Parabéns! Você encontrou o tesouro!",
+            fontSize = 24.sp
+        )
+        Spacer(modifier = Modifier.padding(24.dp))
+        Button(onClick = { onVoltarTelaInicial() }) {
+            Text("Voltar ao início", fontSize = 18.sp)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun MeuPreview(){
-    TelaInicial {  }
+fun MeuPreview() {
+    TelaInicial { }
 }
